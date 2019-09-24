7d3f6a1064434e9a0455627d0906022250864609
Alyssa Yates <bosslys@icloud.com>
2019-09-24 17:54:09 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
+  attr_accessor :name, :experience, :all
 
+  @@all = []
+
+  def initialize
+    @name = name
+    @experience = years
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
