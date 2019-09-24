b1e661435f8a1491dd92e06da1c2dfbde70e03a0
Alyssa Yates <bosslys@icloud.com>
2019-09-24 17:23:29 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,14 +1,12 @@
 class Customer
-  attr_accessor :name, :age, :all 
+  attr_accessor :name, :age, :all
 
   @@all = []
 
-  
+
   def initialize(name, age)
     @name = name
     @age = age
-    
-  end 
+
+  end
 end
-
-
